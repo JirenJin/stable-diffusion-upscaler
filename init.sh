0f3d8b7b86f14d8d71515183f312bb4b3118e8e1
Jiren Jin <8536399+JirenJin@users.noreply.github.com>
2023-12-25 01:59:40 +0900

    Add workable but dirdy code.

---
+++ init.sh
@@ -0,0 +1,5 @@
+pip install -r requirements.txt
+
+git clone https://github.com/CompVis/stable-diffusion
+git clone https://github.com/CompVis/taming-transformers
+git clone https://github.com/CompVis/latent-diffusion
